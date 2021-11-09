3678688d3a3e5c251ba373d08180f8944c2f61e7
EC2 Default User <ec2-user@ip-172-31-57-136.ec2.internal>
2021-11-09 18:24:25 +0000

    Search function

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
   resources :book_comments, only: [:create, :destroy]
   end
+  get '/search', to: 'searchs#search'
 end
