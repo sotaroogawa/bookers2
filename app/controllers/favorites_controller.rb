6082e7294aed59a5918673fe09f5582048d4f104
EC2 Default User <ec2-user@ip-172-31-57-136.ec2.internal>
2021-11-10 13:29:34 +0000

    いいね機能非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: @book.id)
     #favorite = Favorite.new(book_id: book.id)
     #favorite.user_id = current_user.id
-    favorite.save
-   redirect_back(fallback_location: root_path)
+    @favorite.save
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
-    favorite.destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: @book.id)
+    @favorite.destroy
   end
 end
